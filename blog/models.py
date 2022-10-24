2f0575d7a9b4f63d4b3f5b6a0700d8b0727f0426
teerawatth <teerawat.th.64@ubu.ac.th>
2022-10-24 19:39:28 +0700

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,16 @@
 from django.db import models
 
+class bugatti(models.Model):
+    name = models.CharField
+    price = models.CharField
+    desc = models.TextField
+    img = models.CharField
+    img1 = models.CharField
+    img2 = models.CharField
+    img3 = models.CharField
+    img4 = models.CharField
+    img5 = models.CharField
+    img6 = models.CharField
+
 
 
