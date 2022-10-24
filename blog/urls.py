c304886f581ef18e813afbbf9f89488450704a9a
teerawatth <teerawat.th.64@ubu.ac.th>
2022-10-24 22:00:01 +0700

    Initial commit

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('',index,name='index'),
     path('bugatti',buga,name='bugatti'),
     path('bugadetail/<int:pk>',bugadetail,name = 'bugadetail'),
-    path('koenig',koenig,name='koenig'),
+    path('koenigsegg',koenig,name='koenigsegg'),
     path('koenigdetail/<int:pk>',koenigdetail,name = 'koenigdetail'),
 ]
