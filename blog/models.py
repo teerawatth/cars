e2682a5b295121d7e6cb316d582714e6eeae5cf2
teerawatth <teerawat.th.64@ubu.ac.th>
2022-10-24 19:54:12 +0700

    Initial commit

--- blog/models.py
+++ blog/models.py
@@ -1,16 +1,16 @@
 from django.db import models
 
 class bugatti(models.Model):
-    name = models.CharField
-    price = models.CharField
-    desc = models.TextField
-    img = models.CharField
-    img1 = models.CharField
-    img2 = models.CharField
-    img3 = models.CharField
-    img4 = models.CharField
-    img5 = models.CharField
-    img6 = models.CharField
+    name = models.TextField(max_length=100,null=True)
+    price = models.TextField(max_length=100, null=True)
+    desc = models.TextField(null=True)
+    img = models.TextField(null=True)
+    img1 = models.TextField(null=True)
+    img2 = models.TextField(null=True)
+    img3 = models.TextField(null=True)
+    img4 = models.TextField(null=True)
+    img5 = models.TextField(null=True)
+    img6 = models.TextField(null=True)
 
 
 
