895cc128d2aec5d3f90fac74b9f7c443c38baba4
teerawatth <teerawat.th.64@ubu.ac.th>
2022-10-24 20:05:24 +0700

    Initial commit

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 def index(request):
     return render(request,'index.html')
 
 def buga(request):
-    return render(request,'buga.html')
+    bugattis = bugatti.objects.all()
+    return render(request,'buga.html',{
+        'bugattis':bugattis
+    })
 
 def bugadetail(request):
     return render(request,'bugadetail.html')
